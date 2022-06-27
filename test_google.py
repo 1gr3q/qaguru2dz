ad317cba8d68e27f1d859ed2febf07b589df720e
igr3k <igr3k@duck.com>
2022-06-27 14:07:30 +0200

    changed test_false

--- test_google.py
+++ test_google.py
@@ -8,5 +8,5 @@ def test_true(browser_size):
 
 def test_false(browser_size):
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka selene').press_enter()
+    browser.element('[name="q"]').should(be.blank).type('selenium').press_enter()
     browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
